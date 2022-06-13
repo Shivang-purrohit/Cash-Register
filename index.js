617a8de98faf422d266ac4720eb435f6e54f2cab
Shivang Purrohit <104918730+Shivang-purrohit@users.noreply.github.com>
2022-06-13 16:08:04 +0530

    updated with css

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("error-message");
+const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
  checkButton.addEventListener("click", function validateBillAndCashAmount()
  {
-     //hideMessage();
+     hideMessage();
      if(billAmount.value >= billAmount.value){
          const amountToBeReturned = cashGiven.value - billAmount.value;
          calculateChange(amountToBeReturned);
@@ -17,10 +17,10 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
          showMessage("do you wanna wash paltes?");
 
      }
-     //else{
+   // else{
          showMessage("invalid Bill Amount");
 
-     //}
+    // }
      
  });
 
@@ -28,19 +28,19 @@ const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
       for(let i = 0; i < availableNotes.length; i++)
       {
           const numberOfNotes = Math.trunc(
-              amountToBeReturned / availableNotes[i]
+              amountToBeReturned / availableNotes[i]);
 
-          );
           amountToBeReturned = amountToBeReturned % availableNotes[i];
           noOfNotes[i].innerText = numberOfNotes;
+         
 
       }
   }
 
-  //function hideMessage() {
-     // message.style.display = "none";
-  //} 
- // function showMessage(msg) {
+  function hideMessage() {
+      message.style.display = "none";
+  } 
+  //function showMessage(msg) {
     //  message.style.display = "block";
-    //  message.innerText = msg;
- // }
+     // message.innerText = msg;
+ //}
